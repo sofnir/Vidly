f645596b4443c7a530c99dc80e6781dd6cd07cec
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-12 15:45:23 +0100

    implements customers and movies views

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> Customers = new List<Customer>()
+        {
+            new Customer() { Id = 1, Name = "John Smith" },
+            new Customer() { Id = 2, Name = "Mary Williams" }
+        };
+
+        public IActionResult Index()
+        {            
+            return View(Customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = Customers?.Where(q => q.Id == id)?.FirstOrDefault();
+
+            if (customer == null)
+                return NotFound();
+
+            return Content(customer.Name);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movie> Movies = new List<Movie>()
+        { 
+            new Movie() { Id = 1, Name = "Shrek" },
+            new Movie() { Id = 2, Name = "Wall-e" }
+        };
+
+        public IActionResult Index()
+        {            
+            return View(Movies);
+        }        
+    }
+}

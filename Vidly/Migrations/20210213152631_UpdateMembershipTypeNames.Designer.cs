3a0385dea0f52cc32af938e1a76e86aba17170e7
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-13 16:46:57 +0100

    add membership type name to vie

--- Vidly/Migrations/20210213152631_UpdateMembershipTypeNames.Designer.cs
+++ Vidly/Migrations/20210213152631_UpdateMembershipTypeNames.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vidly.Data;
 
 namespace Vidly.Migrations
 {
     [DbContext(typeof(VidlyContext))]
-    partial class VidlyContextModelSnapshot : ModelSnapshot
+    [Migration("20210213152631_UpdateMembershipTypeNames")]
+    partial class UpdateMembershipTypeNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Vidly/Migrations/20210213152631_UpdateMembershipTypeNames.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class UpdateMembershipTypeNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Pay as You Go' WHERE DurationInMonths = 0");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE DurationInMonths = 1");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE DurationInMonths = 3");
+            migrationBuilder.Sql("UPDATE MembershipTypes SET Name = 'Annual' WHERE DurationInMonths = 12");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

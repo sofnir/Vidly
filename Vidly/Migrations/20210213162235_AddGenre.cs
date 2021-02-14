b440f36a6a2d5d4bd31373b5a103aaa15cd477fe
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-14 15:19:29 +0100

    add genres

---
+++ Vidly/Migrations/20210213162235_AddGenre.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class AddGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var table = "Genres";
+            var column = "Name";
+
+            migrationBuilder.CreateTable(
+                name: table,
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+            
+            migrationBuilder.InsertData(
+                table: table,
+                column: column,
+                values: new object[]
+                {
+                    "Comedy",
+                    "Action",
+                    "Family",
+                    "Romance"
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}

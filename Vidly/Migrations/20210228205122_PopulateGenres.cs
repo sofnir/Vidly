0888358d550b0cb61bc02586258f6628bed6feb3
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-02-28 21:57:07 +0100

    populate genres

---
+++ Vidly/Migrations/20210228205122_PopulateGenres.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Migrations
+{
+    public partial class PopulateGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Genres", "Name", new object[] {
+                "Comedy",
+                "Action",
+                "Family",
+                "Romance"
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("Genres", "Name", new object[] {
+                "Comedy",
+                "Action",
+                "Family",
+                "Romance"
+            });
+        }
+    }
+}

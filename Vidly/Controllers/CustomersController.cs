ab3d593095f3365040c579dcc6739c6f98c90ebe
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-03-02 18:51:32 +0100

    implement edit customer form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -41,19 +41,50 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        public async Task<IActionResult> New()
+        public async Task<IActionResult> Form()
         {
             var membershipTypes = await _context.MembershipTypes.ToListAsync();
-            var newCustomer = new NewCustomerViewModel { MembershipTypes = membershipTypes };
+            var newCustomer = new CustomerFormViewModel { MembershipTypes = membershipTypes };
 
             return View(newCustomer);
         }
 
-        public async Task<IActionResult> Create(Customer customer)
+        [HttpPost]
+        public async Task<IActionResult> Save(Customer customer)
         {
-            await _context.Customers.AddAsync(customer);
+            if(customer.Id == 0)
+                await _context.Customers.AddAsync(customer);
+            else
+            {
+                var customerInDb = await _context.Customers.SingleAsync(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;                
+            }
+
             await _context.SaveChangesAsync();
+
             return RedirectToAction("Index", "Customers");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();
+
+            var membershipTypes = await _context.MembershipTypes.ToListAsync();
+
+            var formCustomer = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = membershipTypes
+            };
+
+            return View("Form", formCustomer);
         }
     }
 }

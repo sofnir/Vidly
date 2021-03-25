e1022acc2fd82b2faa047894ca081e7b32a1bf5c
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2021-03-25 09:58:26 +0100

    create new rental api

---
+++ Vidly/Controllers/Api/RentalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<CustomerDto> NewRental(NewRentalDto newRentalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
